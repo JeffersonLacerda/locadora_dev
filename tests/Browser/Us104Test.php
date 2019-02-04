ddb0873c7dd8c8759a12e64df5679cf7a882d7c6
wfns-cin <wfns@cin.ufpe.br>
2019-02-04 15:40:28 -0300

    Adicionado testes de aceitacao

--- tests/Browser/Us104Test.php
+++ tests/Browser/Us104Test.php
@@ -5,19 +5,30 @@ namespace Tests\Browser;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\User;
 
 class Us104Test extends DuskTestCase
 {
-    /**
-     * A Dusk test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    public function testShowClientOK()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+            $browser->loginAs(User::find(2))
+                    ->visit('/cliente')
+                    ->waitForText('Clientes')
+                    ->keys("#DataTables_Table_0_filter input", 'cliente@teste01.com', '{enter}')
+                    ->assertSee('Cliente Teste 01')
+                    ->assertSee('00000000000')
+                    ->assertSee('87-09876-6789')
+                    ->assertSee('0');
+        });
+    }
+
+    public function testShowClientForbidden()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs(User::find(1))
+                    ->visit('/cliente')
+                    ->assertSee('Acesso Negado.');
         });
     }
 }
